a910b85961ff3fd66b08678e72065dd24e6c1b22
Sebastiaan Moeys <seb.moeys@gmail.com>
2014-05-26 23:24:12 +0200

    Built some stuff

---
+++ code.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+	$('.pin-description').each(function(){
+		$clamp(this, {clamp: 3});
+	});
+
+	resizeVideo();
+	$(window).resize(function() {
+		resizeVideo();
+	});
+
+	shame();
+});
+
+function resizeVideo () {
+	var video = $('.video');
+	video.width('100%');
+	video.height(video.width() * 9 / 16);
+
+	var video = $('.event-video');
+	video.width('100%');
+	console.log(video.width());
+	video.height(video.width() * 9 / 16);
+}
+
+var i = 0;
+
+function shame() {
+	setTimeout(function(){
+		i++;
+		$('.typekit-badge').hide();
+		if(i < 40) {
+			shame();
+		}
+	}, 100);
+}
